1547ae5966f7e6762a05d16405b5e3061af6ff22
Aladser <aladser@gmail.com>
2024-04-18 12:28:19 +0900

    update api

--- src/api/hh_api.py
+++ src/api/hh_api.py
@@ -3,25 +3,23 @@ from src.api.basic_api import BasicApi
 from src import Parser
 
 
-class HHApi(BasicApi, Parser):
+class HHApi(BasicApi):
     """
-        Класс для работы с API HeadHunter
-    `   :param file_worker: рабочий JSON-файл
-        :param page_count: число страниц
-        :param per_page: число вакансий на странице`
+        Класс для работы с API HeadHunter -
+        подключается к HeadHunter и возвращает список найденных вакансий
     """
 
     __url = 'https://api.hh.ru/vacancies'
     __headers = {'User-Agent': 'HH-User-Agent'}
-
     __page_count: int
     __per_page: int
     __params: dict
-    __vacancies: list
 
-    def __init__(self, file_worker):
+    def __init__(self):
         self.__page_count = 1
+        """число страниц"""
         self.__per_page = 30
+        """число вакансий на странице"""
         self.__params = {
             'page': 0,
             'per_page': self.__per_page,
@@ -29,8 +27,6 @@ class HHApi(BasicApi, Parser):
             'area': 113,
             'text': ''
         }
-        self.__vacancies = []
-        super().__init__(file_worker)
 
     def load_vacancies(self, keyword, job_count=None, salary=None) -> list:
         # установка числа вакансий
@@ -46,12 +42,7 @@ class HHApi(BasicApi, Parser):
             vacancies = response.json()['items']
             vacancies_obj.extend(vacancies)
             self.__params['page'] += 1
-        self.__vacancies = self.parse_obj_to_vacancy_cls_copy(vacancies_obj)
-        return self.__vacancies
-
-    @property
-    def vacancies(self) -> list:
-        return self.__vacancies
+        return Parser.parse_obj_to_vacancy_cls_copy(vacancies_obj)
 
     @property
     def params(self) -> str:
--- tests/test_hh.py
+++ tests/test_hh.py
@@ -2,24 +2,13 @@ import pytest
 from src.api import HHApi
 
 
-@pytest.fixture()
-def vacancies_json_path():
-    return '../data/vacancies.json'
-
-
 @pytest.fixture
-def api(vacancies_json_path):
-    return HHApi(vacancies_json_path)
-
-
-def test_init(vacancies_json_path):
-    hh_api = HHApi(vacancies_json_path)
-
-    assert hh_api.vacancies == []
+def api():
+    return HHApi()
 
 
 def test_work(api):
     vacancies_list = api.load_vacancies('PHP разработчик')
     assert len(vacancies_list) == 30
     print()
-    [print(f"{vcn}\n") for vcn in api.vacancies]
+    [print(f"{vcn}\n") for vcn in vacancies_list]
